9caa466913dbb907654a6612e5ec9247127e6c33
neha-peddinti <neha.peddinti@gmail.com>
2025-02-08 12:45:23 -0500

    mongo connection

--- auth0/src/train.py
+++ auth0/src/train.py
@@ -6,9 +6,12 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 # Set up MongoDB connection
-client = MongoClient('mongodb://localhost:27017/') # Add mongodb url
-db = client['database-name'] 
-collection = db['collection-name'] 
+client = MongoClient('mongodb+srv://neha:mongodb321!@walletwhisperer.gykjp.mongodb.net/') # Add mongodb url
+print("connected to mongo")
+databaseName = ''
+collectionName = ''
+db = client[databaseName] 
+collection = db[collectionName] 
 
 # DataFrame from MongoDB
 data = collection.find()
@@ -16,20 +19,21 @@ df = pd.DataFrame(data)
 columns = df.columns.tolist()
 
 # Merchant and Purpose are categorical variables, Satisfaction is 1-5 scale
-X = df[['Price', 'Merchant', 'Purpose']]
-y = df['Satisfaction']  # Target variable
+X = df[['price', 'merchant', 'purpose']]
+print(df.head())
+y = df['satisfaction']  # Target variable
 
 # Encode categorical variables (merchant and purpose)
-X['Merchant'] = LabelEncoder().fit_transform(X['Merchant'])
-X['Purpose'] = LabelEncoder().fit_transform(X['Purpose'])
+X['merchant'] = LabelEncoder().fit_transform(X['merchant'])
+X['purpose'] = LabelEncoder().fit_transform(X['purpose'])
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
 # Standardize price
 scaler = StandardScaler()
-X_train[['Price']] = scaler.fit_transform(X_train[['Price']])
-X_test[['Price']] = scaler.transform(X_test[['Price']])
+X_train[['price']] = scaler.fit_transform(X_train[['price']])
+X_test[['price']] = scaler.transform(X_test[['price']])
 
 # Train with logistic regression
 model = LogisticRegression()
